7cc92dbb6159791a291a198cce25c5648f338bbe
NoelMinogue <noelminogue97@gmail.com>
2018-10-25 19:56:26 +0100

    Update assignment.c

--- CT331_Assignment1-master/src/q1/assignment.c
+++ CT331_Assignment1-master/src/q1/assignment.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
 int main(int arg, char* argc[]){
-  printf("Hello assignment1.\n");
+ int a;
+ int* b;
+ long c;
+ double* d;
+  char** e;
+  printf("size of int = %d", sizeof(a));
+  printf("size of int* = %d", sizeof(b));
+  printf("size of long = %d", sizeof(c));
+  printf("size of double* = %d", sizeof(d));
+  printf("size of char** = %d", sizeof(e));
+ 
+  
+ 
   return 0;
 }
