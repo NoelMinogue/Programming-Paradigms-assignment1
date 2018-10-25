8785ef3cbdae20dc83c4a3632624415855ed98c0
NoelMinogue <noelminogue97@gmail.com>
2018-10-25 19:41:03 +0100

    Update tests.c

--- CT331_Assignment1-master/src/q2/tests.c
+++ CT331_Assignment1-master/src/q2/tests.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include "tests.h"
-#include "linkedList.h"
+#include "LinkedList.h"
+#include "LinkedList.c"
+#include "tests.h"
+#include "LinkedList.h"
+
 
 void runTests(){
   printf("Tests running...\n");
   listElement* l = createEl("Test String (1).", 30);
-  //printf("%s\n%p\n", l->data, l->next);
+
+
   //Test create and traverse
   traverse(l);
   printf("\n");
@@ -21,5 +26,23 @@ void runTests(){
   traverse(l);
   printf("\n");
 
-  printf("\nTests complete.\n");
+  //Returns the number of elements in a linked list  
+  printf("Length of List: %d \n", length(l));
+  
+
+ 
+  listElement** dp = &l;
+  push(dp , "Please work", 30);
+  printf("size after push: %d \n", length(*dp));
+
+  
+  
+  
+  pop(dp);
+  prinf("size after pop: %d \n", length(*dp));
+
+
+
+
+  printf("\nTests finished.\n");
 }
